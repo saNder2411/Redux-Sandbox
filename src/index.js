41da690907ed19ab9333dd72ef01c8f7598258d5
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-25 20:53:08 +0300

    Create custom bindActionCreator.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 import './bootstrap.min.css';
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 import {dec, inc, rndDec, rndInc} from './actions'
 import reducer from './reducer';
 
 const store = createStore(reducer);
+const {dispatch, getState} = store;
+
+const bindActionCreator = (actionCreator, dispatch) => (...args) => {
+  dispatch(actionCreator(...args));
+};
+
+const decDispatch = bindActionCreators(dec, dispatch);
+const incDispatch = bindActionCreator(inc, dispatch);
+
+const {rndDecDispatch, rndIncDispatch} = bindActionCreators({
+  rndDecDispatch: rndDec,
+  rndIncDispatch: rndInc,
+}, dispatch)
 
 document.getElementById(`dec`)
-  .addEventListener(`click`, () => store.dispatch(dec()));
+  .addEventListener(`click`, decDispatch);
 
 document.getElementById(`inc`)
-  .addEventListener(`click`, () => store.dispatch(inc()));
+  .addEventListener(`click`, incDispatch);
 
 document.getElementById(`rnd-dec`)
-  .addEventListener(`click`, () => store.dispatch(rndDec(Math.floor(Math.random() * 11))));
+  .addEventListener(`click`, () => rndDecDispatch(Math.floor(Math.random() * 11)));
 
 document.getElementById(`rnd-inc`)
-  .addEventListener(`click`, () => store.dispatch(rndInc(Math.floor(Math.random() * 11))));
+  .addEventListener(`click`, () => rndIncDispatch(Math.floor(Math.random() * 11)));
 
 const update = () => {
   document.getElementById(`counter`)
-  .textContent = store.getState();
+  .textContent = getState();
 };
 
 store.subscribe(update);
